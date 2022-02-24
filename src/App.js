456dbbcf10ceb62a8673644fa7c77717ed07f07e
Jack <jacktech.system@gmail.com>
2022-02-24 19:53:40 -0300

    Css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./App.css";
+import Timer from "./Timer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Pomodoro App</h1>
+      <div className="main">
+        <Timer />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Timer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Timer = () => {
+  return (
+    <div className="Timer">
+      <span className="Timer-span">25:00</span>
+
+      <button>Start</button>
+    </div>
+  );
+};
+
+export default Timer;
