395884811c985b51f48536fa4f0bd97e56217a09
Jack <jacktech.system@gmail.com>
2022-02-24 20:23:21 -0300

    Bug fixes

--- src/Timer.js
+++ src/Timer.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 const Timer = () => {
-  const [sec, setSec] = React.useState(0 * 60 + 4); //25 minutos.
+  const [sec, setSec] = React.useState(25 * 60); //25 minutos.
+  const [click, setClick] = React.useState(false);
   const min = Math.floor(sec / 60); //Um minuto
   const seconds = sec % 60;
 
@@ -14,9 +15,15 @@ const Timer = () => {
       setSec(25 * 60);
       alert("Acabou!");
     }
-  }, [sec]);
+  }, [sec, click]);
 
-  function handleTimer() {}
+  function handleTimer() {
+    setClick(!click);
+
+    if (click === true) {
+      return;
+    }
+  }
 
   return (
     <div className="Timer">
