cb7a32023da117c50006cfe03397f24499e627c7
Jack <jacktech.system@gmail.com>
2022-02-24 20:17:57 -0300

    Timer

--- src/Timer.js
+++ src/Timer.js
@@ -1,11 +1,30 @@
 import React from "react";
 
 const Timer = () => {
+  const [sec, setSec] = React.useState(0 * 60 + 4); //25 minutos.
+  const min = Math.floor(sec / 60); //Um minuto
+  const seconds = sec % 60;
+
+  React.useEffect(() => {
+    if (sec > 0) {
+      var count = setTimeout(() => {
+        setSec(sec - 1);
+      }, 1000);
+    } else {
+      setSec(25 * 60);
+      alert("Acabou!");
+    }
+  }, [sec]);
+
+  function handleTimer() {}
+
   return (
     <div className="Timer">
-      <span className="Timer-span">25:00</span>
+      <span className="Timer-span">
+        {String(min).padStart(2, "0")}:{String(seconds).padStart(2, "0")}
+      </span>
 
-      <button>Start</button>
+      <button onClick={handleTimer}>Start</button>
     </div>
   );
 };
